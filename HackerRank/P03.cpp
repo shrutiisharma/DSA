ed946cb9500dfa5de708228db57cc5bf648f3807
Shruti Sharma <72591283+shrutiisharma@users.noreply.github.com>
2020-10-27 00:33:09 +0530

    Basic Data Types

---
+++ HackerRank/P03.cpp
@@ -0,0 +1,24 @@
+//Basic Data Types
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+
+    cin>>a>>b>>c>>d>>e;
+
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c<<endl;
+    
+    printf("%.3f \n",d);
+    printf("%.9lf",e);
+    
+    return 0;
+}
