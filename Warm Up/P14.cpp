69490e42a680488d9180d5dfe42f744876245aeb
Shruti Sharma <72591283+shrutiisharma@users.noreply.github.com>
2020-11-04 22:13:52 +0530

    Sum of even no.s

---
+++ Warm Up/P14.cpp
@@ -0,0 +1,25 @@
+//Program to find sum of all even numbers between 1 to n.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,i=1;
+    int sum=0;
+
+    cout<<"Enter n:";
+    cin>>n;
+
+    while(i<=n)
+    {
+        if(i%2==0)
+        {
+            sum=sum+i;
+        }
+        i++;
+    }
+    cout<<"Sum of all even numbers between 1 to n is: "<<sum;
+
+    return 0;
+}
