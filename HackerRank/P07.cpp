b4d30dec79a7081a49c2242589fd75044cd839fe
Shruti Sharma <72591283+shrutiisharma@users.noreply.github.com>
2020-11-06 18:08:27 +0530

    Pointers

---
+++ HackerRank/P07.cpp
@@ -0,0 +1,20 @@
+//Pointers
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void update(int *a,int *b) {
+    *a= *a + *b;
+    *b= abs(*a - *b - *b);
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+
+    cin>>a>>b;
+    update(pa, pb);
+    cout<<a<<endl<<b;
+
+    return 0;
+}
